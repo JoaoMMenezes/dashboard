23a5b52961a7fbf52e0cab40841253db91d7afdd
JoaoMMenezes <joaopm.m0@gmail.com>
2024-02-19 21:32:30 -0300

    homepage 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,9 +21,9 @@ const NavButton = ({ title, customFunct, icon, color, dotColor }) => (
       <span
         style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-      >
-        {icon}
-      </span>
+      />
+
+      {icon}
     </button>
   </TooltipComponent>
 );
